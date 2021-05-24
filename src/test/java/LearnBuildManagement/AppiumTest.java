425c895059f3e975a0acc6aec7ed55e3f40a71e0
kushw <jitendrakushwah1994@gmail.com>
2021-05-24 23:32:23 +0530

    Architect Demo Memeber

--- src/test/java/LearnBuildManagement/AppiumTest.java
+++ src/test/java/LearnBuildManagement/AppiumTest.java
@@ -7,11 +7,24 @@ public class AppiumTest {
 	@Test
 	public void NativeAndroidApp() {
 		System.out.println("NativeAndroidApp");
+		System.out.println("NativeAndroidApp");
 	}
 	
+	public class AppiumTest2 {
+		
+		@Test
+		public void NativeAndroidApp() {
+			System.out.println("NativeAndroidApp");
+			System.out.println("NativeAndroidApp");
+			System.out.println("NativeAndroidApp");
+		}
 	@Test
 	public void IosApp() {
 		System.out.println("IosApp");
+		System.out.println("IosApp");		
+		System.out.println("IosApp");
+		System.out.println("IosApp");
+		
 	}
 
 }
--- src/test/java/LearnBuildManagement/RestAssuredTest.java
+++ src/test/java/LearnBuildManagement/RestAssuredTest.java
@@ -18,4 +18,8 @@ public class RestAssuredTest {
 	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
 	}
+	@Test
+	public void deleteTwitter1() {
+		System.out.println("deleteTwitter");
+	}
 }
